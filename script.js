75bef46c6c0000b9ed289328bf5c6860a5c3a6f6
limadanilo93 <88122215+limadanilo93@users.noreply.github.com>
2022-03-03 23:40:38 -0300

    feat: main section

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function init() {
   const hamburger = document.querySelector(".hamburguer");
-  const cabecalho = document.querySelectorAll('.cabecalho')
+  const cabecalho = document.querySelector('.cabecalho');
   const navigation = document.querySelector(".collapsable");
   const options = document.querySelector(".options");
   const menu = document.querySelector(".sections__menu");
@@ -11,43 +11,34 @@ function init() {
 
   const tl = new TimelineMax({ paused: true, reversed: true });
 
-  tl.to(navOpen, 0.5, { x: 0 })
-    .fromTo(navOpen, 0.5, { opacity: 0, x: 10 }, { opacity: 1, x: 0 }, "-=0.1")
-    
- 
-    .fromTo(
-        hamburgerLines,
-        0.2,
-        { transform: 'scale(0.8)' },
-        { transform: 'scale(1)'},
-        "-=1"
-      )
-    .fromTo(
-        sidebar,
-        0.2,
-        { width: '50px' },
-        { width: '250px' },
-        "-=1"
-      )
-      .fromTo(
-        navOpen,
-        0.2,
-        { pointerEvents: 'none' },
-        { pointerEvents: 'all' },
-        "-=1"
-      )
-      .fromTo(
-        sidebarCollapsed,
-        0.2,
-        { transform: 'translate(0, 0)', opacity: 1},
-        { transform: 'translate(0,-100%)', opacity: 0},
-        "-=1"
-      )
 
-  cabecalho.addEventListener("click", () => {
-    tl.reversed() ? tl.play() : tl.reverse();
-    
-  });
+if (window.innerWidth<768){
+  tl.to(options, 0.5, { y: 0 })
+  .fromTo(options, 0.2, { opacity: 0}, { opacity: 1}, "-=0.1")
+  .fromTo(
+    options,
+    0.2,
+    { display: 'none' },
+    { display: 'flex' },
+    "-=1"
+  )
+
+  .fromTo(
+      options,
+      0.2,
+      { pointerEvents: 'none' },
+      { pointerEvents: 'all' },
+      "-=1"
+  )
+
+cabecalho.addEventListener("click", () => {
+  tl.reversed() ? tl.play() : tl.reverse();
   
+
+});
+
 }
+ 
+}
+
 init();
