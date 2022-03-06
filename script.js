cc73183a4140c01936c23f9a41ba7742674fd1ef
limadanilo93 <88122215+limadanilo93@users.noreply.github.com>
2022-03-06 01:20:15 -0300

    feat: toggle content mobile

--- script.js
+++ script.js
@@ -3,52 +3,96 @@ const cabecalho = document.querySelector('.cabecalho');
 const navigation = document.querySelector(".collapsable");
 const options = document.querySelector(".options");
 const menu = document.querySelector(".sections__menu");
-const menuItens = document.querySelectorAll(".sections__iten");
+const sections = document.querySelectorAll(".sections")
+const menuItens = document.querySelectorAll(".sections__iten.inheader");
 const mainContent = document.querySelector(".principal");
 const botaoEndereco = document.querySelector(".botao-Endereco");
 const botaoTelefone = document.querySelector(".botao-Celular");
-const tl = new TimelineMax({ paused: true, reversed: true });
+
 function init() {
+  const tl = new TimelineMax({ paused: true, reversed: true });
+/*   const ch = new TimelineMax({ paused: true, reversed: true });
+  menuItens.forEach((item, i) => {
+    let nSections = Array.from(sections)
+    nSections.splice(i, 1);
+    ch.to(sections[i], 0.2, {display: 'flex'}, "-=0.1")
+    .to(nSections, 0.2, {display: 'none'}, "+=0.1")
 
-  if (window.innerWidth<640){
-    tl.to(options, 0.5, { y: 0 })
-    .fromTo(options, 0.2, { opacity: 0}, { opacity: 1}, "-=0.1")
-    .fromTo(
-      options,
-      0.2,
-      { display: 'none' },
-      { display: 'flex' },
-      "-=1"
-    )
 
-    .fromTo(
+    menuItens[i].addEventListener("click", ch.play())
+  }) */
+
+
+    if (window.innerWidth<640){
+      tl.to(options, 0.5, { y: 0 })
+      .fromTo(options, 0.2, { opacity: 0}, { opacity: 1}, "-=0.1")
+      .fromTo(
         options,
         0.2,
-        { pointerEvents: 'none' },
-        { pointerEvents: 'all' },
+        { display: 'none' },
+        { display: 'flex' },
         "-=1"
-    )
-
-  cabecalho.addEventListener("click", () => {
-    tl.reversed() ? tl.play() : tl.reverse();
+      )
+  
+      .fromTo(
+          options,
+          0.2,
+          { pointerEvents: 'none' },
+          { pointerEvents: 'all' },
+          "-=1"
+      )
+  
+    cabecalho.addEventListener("click", () => {
+      tl.reversed() ? tl.play() : tl.reverse();
+      
+  
+    });
+    menuItens.forEach((item, i) => {
+      item.addEventListener("click", toggle)
+      function toggle(){
+        
+        let nSections = Array.from(sections)
+        nSections.splice(i, 1)
+        nSections.forEach(section => section.style.setProperty("display", "none"))
+      
+        sections[i].style.setProperty("display", "flex")
+  
+   
+    }
+    })
+    if (window.innerWidth>=640){
+      menuItens.forEach((item, i) => {
+        item.addEventListener("click", toggle)
+        function toggle(){
+          
+          let nSections = Array.from(sections)
+          nSections.splice(i, 1)
+          nSections.forEach(section => section.style.setProperty("display", "none"))
+        
+          sections[i].style.setProperty("display", "grid")
     
-
-  });
-
-
+     
+      }
+      })
+    }
+  
+  
+    
   }
+  
+  
+  
 
- 
-}
-function reappear(){
-
-  if (window.innerWidth>768){
-  options.style.setProperty("display", "flex");
-  }
+  
 
 }
 init();
-
+function reappear(){
+  
+  if (window.innerWidth>768){
+  options.style.setProperty("display", "flex");
+  }
+}
 screen.orientation.addEventListener('change',  reappear)
 window.onresize = init;
 window.onresize = reappear;
